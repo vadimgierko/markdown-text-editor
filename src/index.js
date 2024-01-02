a3050b245d6b9322d243d8a8f7d67d8f57faecc4
Vadim Gierko <85487884+vadimgierko@users.noreply.github.com>
2024-01-02 22:15:32 +0000

    fix bug: import in body of module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 // css for highlighting code in github style
 // (without this rehype-highlight will not be working):
 import "highlight.js/styles/github.css";
-import App from './App';
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+import App from './App';
+import EditorPage from './pages/EditorPage';
+import AboutPage from './pages/AboutPage';
 import RenderedHtmlPage from './pages/RenderedHtmlPage';
 
 const router = createBrowserRouter([
@@ -36,10 +41,6 @@ const router = createBrowserRouter([
   {basename: process.env.PUBLIC_URL}
 );
 
-
-import ReactDOM from 'react-dom/client';
-import EditorPage from './pages/EditorPage';
-import AboutPage from './pages/AboutPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
